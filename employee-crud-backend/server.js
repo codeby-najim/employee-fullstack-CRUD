39f169fa11f5c43be5205dadd9fc0730cd294ba5
codeby-najim <najimudeen06@gmail.com>
2025-07-22 00:36:48 +0530

    fix: dynamic port for Render

--- employee-crud-backend/server.js
+++ employee-crud-backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const PORT = 5000;
+const PORT = process.eventNames.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
